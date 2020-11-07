10602c85519836ba5669f4b279735956a5807b96
mrelectrq <tiora100bass@gmail.com>
2020-11-07 20:15:00 +0200

    init

---
+++ EliteShoppyFunctionalityTests/CAL_174.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace EliteShoppyFunctionalityTests
+{
+    class CAL_174
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
